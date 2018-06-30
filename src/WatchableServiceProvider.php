6a2130020bcee5f86a871cac200613e04e473b29
Shipu Ahamed <shipuahamed01@gmail.com>
2018-06-30 18:11:55 +0600

    Publish watchable config
    Publish watchable config

--- src/WatchableServiceProvider.php
+++ src/WatchableServiceProvider.php
@@ -4,6 +4,8 @@ namespace Shipu\Watchable;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application as LaravelApplication;
+use Laravel\Lumen\Application as LumenApplication;
 
 class WatchableServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,21 @@ class WatchableServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->publishConfig();
+    }
+
+    /**
+     * Publish config.
+     */
+    protected function publishConfig()
+    {
+        $source = realpath(__DIR__.'/../config/watchable.php');
+        // Check if the application is a Laravel OR Lumen instance to properly merge the configuration file.
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('watchable.php')], 'shipu-watchable-config');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('watchable');
+        }
+        $this->mergeConfigFrom($source, 'watchable');
     }
 }
