3391aba8e8816be444806a4606f99dbbb192c44f
Shipu Ahamed <shipuahamed01@gmail.com>
2018-06-30 18:14:04 +0600

    Update Config
    Update Config

--- config/watchable.php
+++ config/watchable.php
@@ -4,5 +4,8 @@ return [
     'audit_columns' => [
         'creator_column' => 'creator',
         'editor_column' => 'editor'
+    ],
+    'activity_log' => [
+        'model' => \Shipu\Watchable\Models\Activity::class
     ]
 ];
