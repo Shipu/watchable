9a8016fb7b43aa710b42901fc24516b4bad8664e
Md Mashqur Ul Alam <mashqur@deligram.com>
2019-03-25 11:15:41 +0600

    activity helper function exist or not checking updated

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 use Shipu\Watchable\Utility\ActivityLogger;
 
-if(function_exists('activity')) {
+if(!function_exists('activity')) {
     function activity() {
         return app(ActivityLogger::class);
     }
